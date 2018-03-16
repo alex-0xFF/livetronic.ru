303b087ffc6270603392dcf0abd9ca29a1c2cd21
Alexander <9460684+alex-0xFF@users.noreply.github.com>
2018-03-16 12:40:54 +0300

    Убран лишний код

--- LiveTronic Mini/Настройка и тестирование/Тестирование модулей/Bluetooth_Setup/Bluetooth_Setup.ino
+++ LiveTronic Mini/Настройка и тестирование/Тестирование модулей/Bluetooth_Setup/Bluetooth_Setup.ino
@@ -8,30 +8,19 @@
 Разработчик: Артур Голубцов
 *******************************************************************************/
 
-//int BT_SETUP_PIN = A1;                    // Пин, отвечающий за настройку Bluetooth модуля
+int BT_SETUP_PIN = A1;                    // Пин, отвечающий за настройку Bluetooth модуля
 
 void setup() {
-  delay(3000);
-  pinMode(A0, OUTPUT);                     // Режим работы порта - вывод
-  pinMode(A1, OUTPUT);
-  //Serial.begin(115200);                   // Подключаемся к основному последовательному порту платы
-  //digitalWrite(A0, LOW);
-  //digitalWrite(A1, LOW);
-  //delay(1000);
-  Serial.begin(9600);                       // Подключаемся к последовательному порту на котором расположен HC-05 модуль
-  digitalWrite(A0, HIGH);
-  digitalWrite(A1, HIGH);
-  delay(100);
-  Serial.println("AT+NAME=LTLite 000001");    // Здесь можно поменять название модуля
+  delay(3000);  
+  pinMode(BT_SETUP_PIN, OUTPUT);  
+  Serial.begin(9600);                       // Подключаемся к последовательному порту на котором расположен HC-05 модуль  
+  digitalWrite(BT_SETUP_PIN, HIGH);
+  delay(500);
+  Serial.println("AT+NAME=LTMini 000006");    // Здесь можно поменять название модуля
   Serial.println("AT+PSWD=1234");             // Устанавливаем пароль
-  Serial.println("AT+BAUD=9600");             // Устанавливаем скорость обмена данными   
-  digitalWrite(A0, LOW);
-  digitalWrite(A1, LOW);
-  
+  Serial.println("AT+BAUD=9600");             // Устанавливаем скорость обмена данными     
+  digitalWrite(BT_SETUP_PIN, LOW);  
 }
 
 void loop () { 
-  if (Serial.available()) {
-    Serial.write(Serial.read());                              
-  }
 }
