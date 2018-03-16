b628a5c413d1d6d0c7583771a06124d985bb192d
Alexander <9460684+alex-0xFF@users.noreply.github.com>
2018-03-16 12:34:49 +0300

    Убрано чтение A8, которого нет на плате

--- LiveTronic Mini/Настройка и тестирование/Тестирование модулей/Digital_and_Analog_Read_test/Digital_and_Analog_Read_test.ino
+++ LiveTronic Mini/Настройка и тестирование/Тестирование модулей/Digital_and_Analog_Read_test/Digital_and_Analog_Read_test.ino
@@ -23,15 +23,14 @@ void setup() {
   pinMode(A4, INPUT);
   pinMode(A5, INPUT);
   pinMode(A6, INPUT);
-  pinMode(A7, INPUT);
-  pinMode(A8, INPUT);  
+  pinMode(A7, INPUT);  
   Serial.begin(115200);
 }
 
 void loop() {
   
-  Serial.println("D18 D19 D20 D21 D24 D25 A0   A1   A2   A3   A4   A5   A6   A7   A8   ");
-  Serial.printf("%3d %3d %3d %3d %3d %3d %4d %4d %4d %4d %4d %4d %4d %4d %4d\n", digitalRead(18), digitalRead(19), digitalRead(20), digitalRead(21), digitalRead(24), digitalRead(25), analogRead(A0), analogRead(A1), analogRead(A2), analogRead(A3), analogRead(A4), analogRead(A5), analogRead(A6), analogRead(A7), analogRead(A8));
+  Serial.println("D18 D19 D20 D21 D24 D25 A0   A1   A2   A3   A4   A5   A6   A7   ");
+  Serial.printf("%3d %3d %3d %3d %3d %3d %4d %4d %4d %4d %4d %4d %4d %4d %4d\n", digitalRead(18), digitalRead(19), digitalRead(20), digitalRead(21), digitalRead(24), digitalRead(25), analogRead(A0), analogRead(A1), analogRead(A2), analogRead(A3), analogRead(A4), analogRead(A5), analogRead(A6), analogRead(A7));
   delay(200);
 
 }
